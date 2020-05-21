fa810964bac9a4a3860ad85a02aef5506032a042
cesarrivas10 <cesarjose58@gmail.com>
2020-05-21 08:33:41 -0400

    Implemented resolvers

--- index.js
+++ index.js
@@ -2,15 +2,24 @@
 
 const { graphql, buildSchema } = require('graphql');
 
-/* Definiendo el esquema */
+/* Definiendo el esquema: define la api en graphql*/
 const schema = buildSchema(`
   type Query {
     hello: String
+    salute: String
   }
 `);
 
+/* Configurar los resolvers */
+const resolvers = {
+  hello: () => {
+    return 'Hello world';
+  },
+  salute: () => 'Hello everyone'
+};
+
 /* Ejecutar query hello */
-graphql(schema, `{ hello }`)
+graphql(schema, `{ hello salute }`, resolvers)
   .then((data) => {
     console.log(data);
   });
