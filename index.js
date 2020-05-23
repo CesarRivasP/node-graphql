9212caad019dfecd323f2f726652c57f2123c6d6
cesarrivas10 <cesarjose58@gmail.com>
2020-05-23 08:20:46 -0400

    Implemented new library

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict'
 
-const { buildSchema } = require('graphql')
+const { 
+  /* buildSchema */ 
+  makeExecutableSchema  // semejante a buildSchema, pero realiza las tareas de una manera mas especializada
+} = require('graphql-tools')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')  // para leer el archivo de forma asincrona
@@ -11,13 +14,13 @@ const app = express()
 
 const port = process.env.port || 3000
 
-/* Definiendo el esquema: define la api en graphql */
-const schema = buildSchema( // leer el schema
-  readFileSync(
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8'
-  )
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
 )
+
+/* Definiendo el esquema: define la api en graphql */
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', gqlMiddleware({
   schema: schema,
