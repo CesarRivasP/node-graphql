e2a4fa302f662dfb829e36c551d789f01a0688f3
cesarrivas10 <cesarjose58@gmail.com>
2020-05-23 07:40:21 -0400

    Implemented server features

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql');
-const express = require('express');
-const gqlMiddleware = require('express-graphql');
+const { buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')  // para leer el archivo de forma asincrona
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
-const app = express();
+const app = express()
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3000
 
-/* Definiendo el esquema: define la api en graphql*/
-const schema = buildSchema(`
-  type Query {
-    hello: String
-    salute: String
-  }
-`);
+/* Definiendo el esquema: define la api en graphql */
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
 /* Configurar los resolvers */
-const resolvers = {
+/* const resolvers = {
   hello: () => {
-    return 'Hello world';
+    return 'Hello world'
   },
   salute: () => 'Hello everyone'
-};
+} */
 
 /* Ejecutar query hello */
 /* graphql(schema, `{ hello salute }`, resolvers)
@@ -30,12 +33,12 @@ const resolvers = {
     console.log(data);
   }); */
 
-  app.use('/api', gqlMiddleware({
-    schema: schema,
-    rootValue: resolvers, //resolvers que va a ejecutar
-    graphiql: true  //graphiql es el entorno de desarrollo a utilizar
-  }));
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers, // resolvers que va a ejecutar
+  graphiql: true // graphiql es el entorno de desarrollo a utilizar
+}))
 
-  app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}/api`);
-  });
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
